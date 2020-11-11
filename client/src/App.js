c808a2beec3efd96bb679f7e1bf8afad8d62ad14
Ameco <hamecow@gmail.com>
2020-11-10 18:03:21 -0800

    Render webpage, not just list of user api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-//import RobertScene from "./components/RobertScene";
-//import SpeechScene from "./components/SpeechScene";
+import RobertScene from "./components/RobertScene";
+import SpeechScene from "./components/SpeechScene";
 
 // SERVICES
 import robertService from './services/robertService';
@@ -8,19 +8,7 @@ import robertService from './services/robertService';
 import "./App.css";
 
 function App() {
-  //const user = {userId:"1234", returning:true};
-  //const robert = {mood:"helpful", actions:""};
-  //const speechBubble = {talking:true, speechText:"Hello"};
-  //const buttons = {buttonText:["YES", "NO"]}
-  //return (
-  //  <div className="wrapper">
-  //    <RobertScene talking={true}/>
-  //    <SpeechScene robert = {robert} speechBubble = {speechBubble} user = {user} buttons = {buttons}/>
-  //  </div>
-  //);
 
-  
-//}
 const [users, setusers] = useState(null);
 
   useEffect(() => {
@@ -35,26 +23,37 @@ const [users, setusers] = useState(null);
     setusers(res);
   }
 
-  const renderuser = user => {
-    return (
-      <li key={user._id} className="list__item user">
-        <h3 className="user__name">{user.userId}</h3>
-        <p className="user__description">{user.date}</p>
-      </li>
-    );
-  };
-
+  const user = {userId:"1234", returning:true};
+  const robert = {mood:"helpful", actions:""};
+  const speechBubble = {talking:true, speechText:"Hello"};
+  const buttons = {buttonText:["YES", "NO"]}
   return (
-    <div className="App">
-      <ul className="list">
-        {(users && users.length > 0) ? (
-          users.map(user => renderuser(user))
-        ) : (
-          <p>No users found</p>
-        )}
-      </ul>
+    <div className="wrapper">
+      <RobertScene talking={true}/>
+      <SpeechScene robert = {robert} speechBubble = {speechBubble} user = {user} buttons = {buttons}/>
     </div>
   );
+
+  //const renderuser = user => {
+  //  return (
+  //    <li key={user._id} className="list__item user">
+  //      <h3 className="user__name">{user.userId}</h3>
+  //      <p className="user__description">{user.date}</p>
+  //    </li>
+  //  );
+  //};
+
+  //return (
+  //  <div className="App">
+  //    <ul className="list">
+  //      {(users && users.length > 0) ? (
+  //        users.map(user => renderuser(user))
+  //      ) : (
+  //        <p>No users found</p>
+  //      )}
+  //    </ul>
+  //  </div>
+  //);
 }
 
 
