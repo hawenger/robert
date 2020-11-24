bc983e0e48f659cef8e5f507b673e1270477238f
Ameco <hamecow@gmail.com>
2020-11-24 12:19:07 -0800

    Update PORT

--- server.js
+++ server.js
@@ -3,21 +3,23 @@ const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 
-//IMPORT MODELS
 require('./models/SchemaData');
 
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/robert`, { useUnifiedTopology: true } );
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/robert`,  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+} );
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(bodyParser.json());
 
-//IMPORT ROUTES
 require('./routes/robertRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
@@ -29,16 +31,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(express.static("public"));
-// Define API routes here
 
-//mongoose.connect("mongodb+srv://hawenger:waffles@pinkwizard.nje1c.mongodb.net/robertdatabase", { useNewUrlParser: true }, { useUnifiedTopology: true });
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+app.listen(process.env.PORT || 5000);//, () => {
+//  console.log(`🌎 ==> API server now on port ${PORT}!`);
+//});
