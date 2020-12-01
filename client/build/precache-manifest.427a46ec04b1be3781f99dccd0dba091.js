f1091d3def128cae20c1fdda233bdf1d7100c361
Ameco <hamecow@gmail.com>
2020-11-30 19:52:34 -0800

    npm build

---
+++ client/build/precache-manifest.427a46ec04b1be3781f99dccd0dba091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15f0f15ced090722151acfd792992c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0b4357c2529acd378b",
+    "url": "/static/css/main.356c3b54.chunk.css"
+  },
+  {
+    "revision": "16df1c5a1bf1d1500d5e",
+    "url": "/static/js/2.29a22562.chunk.js"
+  },
+  {
+    "revision": "a4d0b4ed9ed777a07755208fd08d27e3",
+    "url": "/static/js/2.29a22562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0b4357c2529acd378b",
+    "url": "/static/js/main.411dfefb.chunk.js"
+  },
+  {
+    "revision": "520bb98303201a0d4d56",
+    "url": "/static/js/runtime-main.a031ffef.js"
+  }
+]);
