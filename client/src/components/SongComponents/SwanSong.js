6a0c35437ba9b286825183a16fc7a5c8ae30d5d1
Hannah Wenger <hamecow@gmail.com>
2022-01-03 23:33:45 -0500

    Add randomness to oscillator type

--- client/src/components/SongComponents/SwanSong.js
+++ client/src/components/SongComponents/SwanSong.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import * as Tone from 'tone';
+import React, { useRef } from "react";
+import * as Tone from "tone";
 
 const SwanSong = () => {
   const playASongButton = useRef();
@@ -21,12 +21,43 @@ const SwanSong = () => {
   let sixthMelody = [];
 
   //Defining all major and minor scales
-  const scales = [['C', 'D', 'E', 'F', 'G', 'A', 'B'], ['G', 'A', 'B', 'C', 'D', 'E', 'F#'], ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'], ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'], ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#'], ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#'], ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#'], ['Cb', 'Db', 'Eb', 'Fb', 'Gb', 'Ab', 'Bb'], ['Gb', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'F'], ['Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb', 'Cb'], ['Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G'], ['Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D'], ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'], ['F', 'G', 'A', 'Bb', 'C', 'D', 'E']];
+  const scales = [
+    ["C", "D", "E", "F", "G", "A", "B"],
+    ["G", "A", "B", "C", "D", "E", "F#"],
+    ["D", "E", "F#", "G", "A", "B", "C#"],
+    ["A", "B", "C#", "D", "E", "F#", "G#"],
+    ["E", "F#", "G#", "A", "B", "C#", "D#"],
+    ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+    ["F#", "G#", "A#", "B", "C#", "D#", "E#"],
+    ["C#", "D#", "E#", "F#", "G#", "A#", "B#"],
+    ["Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb"],
+    ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"],
+    ["Db", "Eb", "F", "Gb", "Ab", "Bb", "Cb"],
+    ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
+    ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
+    ["Bb", "C", "D", "Eb", "F", "G", "A"],
+    ["F", "G", "A", "Bb", "C", "D", "E"],
+  ];
 
   //Defining common chord progressions for a 3 note chord (root, third, sixth);
-  const chordProgressionsRoot = [[0, 4, 5, 3], [4, 5, 3, 0], [5, 3, 0, 4], [3, 0, 4, 5]];
-  const chordProgressionsThird = [[2, 6, 0, 5], [6, 0, 5, 2], [0, 5, 2, 6], [5, 2, 6, 0]];
-  const chordProgressionsSixth = [[4, 1, 2, 0], [1, 2, 0, 4], [2, 0, 4, 1], [0, 4, 1, 2]];
+  const chordProgressionsRoot = [
+    [0, 4, 5, 3],
+    [4, 5, 3, 0],
+    [5, 3, 0, 4],
+    [3, 0, 4, 5],
+  ];
+  const chordProgressionsThird = [
+    [2, 6, 0, 5],
+    [6, 0, 5, 2],
+    [0, 5, 2, 6],
+    [5, 2, 6, 0],
+  ];
+  const chordProgressionsSixth = [
+    [4, 1, 2, 0],
+    [1, 2, 0, 4],
+    [2, 0, 4, 1],
+    [0, 4, 1, 2],
+  ];
 
   //Picking random major/minor scale for song
   function pickRandomScale() {
@@ -48,17 +79,29 @@ const SwanSong = () => {
 
   //Function to grab scale array note placements for rooth, third and sixth
   function pickRandomChordProgressionRoot() {
-    rootArray.push((`${chordProgressionsRoot[sequence1]},${chordProgressionsRoot[sequence2]},${chordProgressionsRoot[sequence3]},${chordProgressionsRoot[sequence4]}`).split(','));
+    rootArray.push(
+      `${chordProgressionsRoot[sequence1]},${chordProgressionsRoot[sequence2]},${chordProgressionsRoot[sequence3]},${chordProgressionsRoot[sequence4]}`.split(
+        ","
+      )
+    );
     //console.log(rootArray);
   }
 
   function pickRandomChordProgressionThird() {
-    thirdArray.push((`${chordProgressionsThird[sequence1]},${chordProgressionsThird[sequence2]},${chordProgressionsThird[sequence3]},${chordProgressionsThird[sequence4]}`).split(','));
+    thirdArray.push(
+      `${chordProgressionsThird[sequence1]},${chordProgressionsThird[sequence2]},${chordProgressionsThird[sequence3]},${chordProgressionsThird[sequence4]}`.split(
+        ","
+      )
+    );
     //console.log(thirdArray);
   }
 
   function pickRandomChordProgressionSixth() {
-    sixthArray.push((`${chordProgressionsSixth[sequence1]},${chordProgressionsSixth[sequence2]},${chordProgressionsSixth[sequence3]},${chordProgressionsSixth[sequence4]}`).split(','));
+    sixthArray.push(
+      `${chordProgressionsSixth[sequence1]},${chordProgressionsSixth[sequence2]},${chordProgressionsSixth[sequence3]},${chordProgressionsSixth[sequence4]}`.split(
+        ","
+      )
+    );
   }
 
   //Call all root chord sequence formulas
@@ -73,23 +116,28 @@ const SwanSong = () => {
   function rootChordSequence() {
     //console.log(randomMoodScale);
     //console.log(rootArray[0]);
-    rootMelody = rootArray[0].map(element => randomMoodScale[parseInt(element)]);
+    rootMelody = rootArray[0].map(
+      (element) => randomMoodScale[parseInt(element)]
+    );
     //console.log(rootMelody);
-  };
+  }
 
   function thirdChordSequence() {
     //console.log(randomMoodScale);
     //console.log(thirdArray[0]);
-    thirdMelody = thirdArray[0].map(element => randomMoodScale[parseInt(element)]);
-
-  };
+    thirdMelody = thirdArray[0].map(
+      (element) => randomMoodScale[parseInt(element)]
+    );
+  }
 
   function sixthChordSequence() {
     //console.log(randomMoodScale);
     //console.log(sixthArray[0]);
-    sixthMelody = sixthArray[0].map(element => randomMoodScale[parseInt(element)]);
+    sixthMelody = sixthArray[0].map(
+      (element) => randomMoodScale[parseInt(element)]
+    );
     //console.log(sixthMelody);
-  };
+  }
 
   let root = [];
   let third = [];
@@ -98,19 +146,28 @@ const SwanSong = () => {
   //Give the root of the chord piano placement (i.e Define placement of note in variance of middle C)
   function rootSynthPlacement() {
     randomScale();
-    root = rootMelody.map(element => element + letterArray[Math.floor(Math.random() * letterArray.length)]);
+    root = rootMelody.map(
+      (element) =>
+        element + letterArray[Math.floor(Math.random() * letterArray.length)]
+    );
     console.log(root);
-  };
+  }
 
   function thirdSynthPlacement() {
     randomScale();
-    third = thirdMelody.map(element => element + letterArray[Math.floor(Math.random() * letterArray.length)]);
+    third = thirdMelody.map(
+      (element) =>
+        element + letterArray[Math.floor(Math.random() * letterArray.length)]
+    );
     console.log(third);
-  };
+  }
 
   function sixthSynthPlacement() {
     randomScale();
-    sixth = sixthMelody.map(element => element + letterArray[Math.floor(Math.random() * letterArray.length)]);
+    sixth = sixthMelody.map(
+      (element) =>
+        element + letterArray[Math.floor(Math.random() * letterArray.length)]
+    );
     console.log(sixth);
   }
 
@@ -131,20 +188,21 @@ const SwanSong = () => {
   let letterArray = [];
 
   function randomScale() {
-    for (var i = 0; i < 10; i++); {
-      x = (Math.floor(Math.random() * 4) + 2);
-      y = (Math.floor(Math.random() * 4) + 2);
-      z = (Math.floor(Math.random() * 4) + 2);
-      a = (Math.floor(Math.random() * 4) + 2);
-      b = (Math.floor(Math.random() * 4) + 2);
-      c = (Math.floor(Math.random() * 5) + 1);
+    for (var i = 0; i < 10; i++);
+    {
+      x = Math.floor(Math.random() * 4) + 2;
+      y = Math.floor(Math.random() * 4) + 2;
+      z = Math.floor(Math.random() * 4) + 2;
+      a = Math.floor(Math.random() * 4) + 2;
+      b = Math.floor(Math.random() * 4) + 2;
+      c = Math.floor(Math.random() * 5) + 1;
     }
     letterArray.push(x, y, z, a, b, c);
-    //console.log(letterArray);    
-  };
-
+    //console.log(letterArray);
+  }
 
   function createSong() {
+    pickRandomOscillator();
     pickRandomScale();
     pickRandomChordProgression();
     defineChordSequence();
@@ -152,6 +210,36 @@ const SwanSong = () => {
   }
 
   randomScale();
+  //Pick random oscillators
+  let randomOscillator1;
+  let randomOscillator2;
+  let randomOscillator3;
+  let randomOscillator4;
+  let randomOscillator5;
+  let randomOscillator6;
+
+  const randomOscillators = [
+    { randomOscillator1 },
+    { randomOscillator2 },
+    { randomOscillator3 },
+    { randomOscillator4 },
+    { randomOscillator5 },
+    { randomOscillator6 },
+  ];
+
+  function pickRandomOscillator() {
+    randomOscillators.forEach((element) => {
+      element = oscillators[Math.floor(Math.random() * oscillators.length)];
+      console.log(element);
+      return element;
+    });
+  }
+  const oscillators = [
+    { type: "sine" },
+    { type: "square" },
+    { type: "triangle" },
+    { type: "sawtooth" },
+  ];
 
   const gain = new Tone.Gain(0.5);
   const gain1 = new Tone.Gain(0.5);
@@ -160,93 +248,93 @@ const SwanSong = () => {
 
   let synth2 = new Tone.DuoSynth({
     voice0: {
-      oscillator: { type: 'sawtooth' },
+      oscillator: randomOscillators[0],
       envelope: {
         attack: 0.1,
         release: 4,
-        releaseCurve: 'linear'
+        releaseCurve: "linear",
       },
       filterEnvelope: {
         baseFrequency: 200,
         octaves: 2,
         attack: 0,
         decay: 0,
-        release: 1000
-      }
+        release: 1000,
+      },
     },
     voice1: {
-      oscillator: { type: 'sine' },
+      oscillator: randomOscillators[2],
       envelope: {
         attack: 0.1,
         release: 4,
-        releaseCurve: 'linear'
+        releaseCurve: "linear",
       },
       filterEnvelope: {
         baseFrequency: 200,
         octaves: 2,
         attack: 0,
         decay: 0,
-        release: 1000
-      }
-    }
+        release: 1000,
+      },
+    },
   });
   let synth = new Tone.DuoSynth({
     voice0: {
-      oscillator: { type: 'sawtooth' },
+      oscillator: randomOscillators[3],
       envelope: {
         attack: 0.1,
         release: 4,
-        releaseCurve: 'linear'
+        releaseCurve: "linear",
       },
       filterEnvelope: {
         baseFrequency: 200,
         octaves: 2,
         attack: 0,
         decay: 0,
-        release: 1000
-      }
+        release: 1000,
+      },
     },
     voice1: {
-      oscillator: { type: 'sine' },
+      oscillator: randomOscillators[4],
       envelope: {
         attack: 0.1,
         release: 4,
-        releaseCurve: 'linear'
+        releaseCurve: "linear",
       },
       filterEnvelope: {
         baseFrequency: 200,
         octaves: 2,
         attack: 0,
         decay: 0,
-        release: 1000
-      }
-    }
+        release: 1000,
+      },
+    },
   });
 
   let envelope = {
     attack: 0.1,
     release: 4,
-    releaseCurve: 'linear'
+    releaseCurve: "linear",
   };
   let filterEnvelope = {
     baseFrequency: 200,
     octaves: 2,
     attack: 0,
     decay: 0,
-    release: 1000
+    release: 1000,
   };
 
   let synth1 = new Tone.DuoSynth({
     voice0: {
-      oscillator: { type: 'sawtooth' },
+      oscillator: randomOscillators[5],
       envelope,
-      filterEnvelope
+      filterEnvelope,
     },
     voice1: {
-      oscillator: { type: 'sine' },
+      oscillator: randomOscillators[1],
       envelope,
-      filterEnvelope
-    }
+      filterEnvelope,
+    },
   });
 
   synth.connect(gain);
@@ -292,30 +380,30 @@ const SwanSong = () => {
   //    index++;
   //});
 
-  new Tone.Loop(time => {
+  new Tone.Loop((time) => {
     const notes = root;
     let note = notes[index % notes.length];
-    synth.triggerAttackRelease(note, '8n', time);
+    synth.triggerAttackRelease(note, "8n", time);
     index++;
-  }, '8n').start();
+  }, "8n").start();
 
-  new Tone.Loop(time => {
+  new Tone.Loop((time) => {
     const notes2 = third;
     let note2 = notes2[index % notes2.length];
-    synth1.triggerAttackRelease(note2, '8n', time);
+    synth1.triggerAttackRelease(note2, "8n", time);
     index++;
-  }, '8n').start();
+  }, "8n").start();
 
-  new Tone.Loop(time => {
+  new Tone.Loop((time) => {
     const notes3 = sixth;
     let note3 = notes3[index % notes3.length];
-    synth2.triggerAttackRelease(note3, '8n', time);
+    synth2.triggerAttackRelease(note3, "8n", time);
     index++;
-  }, '8n').start();
+  }, "8n").start();
 
   const playASong = () => {
     createSong();
-    Tone.start()
+    Tone.start();
     Tone.Transport.start();
     //setTimeout(() => {
     //    Tone.Transport.stop();
@@ -324,13 +412,47 @@ const SwanSong = () => {
 
   const stopASong = () => {
     Tone.Transport.stop();
-  }
+  };
 
   return (
     <>
-      <a-entity ref={playASongButton} onClick={playASong} id="stopTheSongButton" geometry="primitive: cone; height: 2; radiusBottom: 0.1; radiusTop: 1" scale="" position="1.2 0.859 -0.5" material="src: #flowers; transparent: true; color: #a3d1ab" id="stopSongButton" rotation="0 180 0"><a-entity id="spheretop" geometry="primitive: sphere; radius: .6" position="0 1 0" material="src: #strawberry"></a-entity></a-entity>
-      <a-entity ref={stopASongButton} onClick={stopASong} id="startTheSongButton" geometry="primitive: cone; height: 2; radiusBottom: 0.1; radiusTop: 1" scale="" position="3.4 0.859 0" material="src: #flowers; transparent: true; color: #a3d1ab" id="startSongButton" rotation="0 180 0"><a-entity id="spheretop" geometry="primitive: sphere; radius: .6" position="0 1 0" material="src: #strawberry"></a-entity></a-entity>
+      <a-entity
+        ref={playASongButton}
+        onClick={playASong}
+        id="stopTheSongButton"
+        geometry="primitive: cone; height: 2; radiusBottom: 0.1; radiusTop: 1"
+        scale=""
+        position="1.2 0.859 -0.5"
+        material="src: #flowers; transparent: true; color: #a3d1ab"
+        id="stopSongButton"
+        rotation="0 180 0"
+      >
+        <a-entity
+          id="spheretop"
+          geometry="primitive: sphere; radius: .6"
+          position="0 1 0"
+          material="src: #strawberry"
+        ></a-entity>
+      </a-entity>
+      <a-entity
+        ref={stopASongButton}
+        onClick={stopASong}
+        id="startTheSongButton"
+        geometry="primitive: cone; height: 2; radiusBottom: 0.1; radiusTop: 1"
+        scale=""
+        position="3.4 0.859 0"
+        material="src: #flowers; transparent: true; color: #a3d1ab"
+        id="startSongButton"
+        rotation="0 180 0"
+      >
+        <a-entity
+          id="spheretop"
+          geometry="primitive: sphere; radius: .6"
+          position="0 1 0"
+          material="src: #strawberry"
+        ></a-entity>
+      </a-entity>
     </>
-  )
-}
+  );
+};
 export default SwanSong;
